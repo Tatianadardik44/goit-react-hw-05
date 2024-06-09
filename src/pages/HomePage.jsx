b360d0e0fea182d3cf6f92473ddf75974d86ba38
Tatianadardik44 <dardiktatiana@gmail.com>
2024-06-09 11:03:45 +0200

    posters

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,34 +1,4 @@
-// import { useEffect, useState } from "react"
-// import { fetchList } from "../components/Gallery/Gallery";
-// import { Link } from "react-router-dom";
-// const HomePage = () => {
-//     const [movies, setMovies] = useState([]);
-//     useEffect(() => {
-//         fetchList()
-//             .then(data => {
-//                 if (data && data.results) {
-//                     setMovies(data.results);
-//                    }
-//             }).catch();
-        
-//     },[])
-//     return (
-//         <div>
-//             <p>Trending today</p>
-//             <div>
-//                 <ul>
-//                     {movies.map(movie => (
-//                         <li key={movie.id}>
-//                                  <Link to={`${movie.id}`}>{movie.title}</Link>
-//                                 </li>
-//                     ))}
-//                     </ul>
-//             </div>
-//         </div>
-//     )
-// }
-// export default HomePage
-// src/pages/HomePage.js
+
 import { useEffect, useState } from "react";
 
 import { Link } from "react-router-dom";
@@ -51,7 +21,7 @@ const HomePage = () => {
 
   return (
     <div>
-      <p>Популярное сегодня</p>
+      <p>Trending today</p>
       <div>
         <ul>
           {movies.map(movie => (
--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -1,40 +1,8 @@
-// import { useParams } from "react-router-dom"
 
-// import { useEffect, useState } from "react";
-// import { fetchMovieById } from "../components/Gallery/Gallery";
-// const MovieDetailsPage = () => {
-//     const  {movieId }= useParams();
-//     console.log(movieId);
-//     const [movie, setMovie] = useState(null);
-   
-//     useEffect(() => {
-      
-//         const getData = async () => {
-//             try {
-//                 const data = await fetchMovieById(movieId)
-//                 setMovie(data);
-//             } catch (error) {
-//                console.log("error");
-//             }
-            
-//         };
-//         getData();
-//     },[movieId]);
-//     return (
-//         <div>
-        
-//                 <h1>{movie.title}</h1>
-//         <p>hello</p>
-//         </div>
-//     )
-   
-// }
-// export default MovieDetailsPage
-// src/pages/MovieDetailsPage.js
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { fetchMovieById } from "../components/Gallery/Gallery";
-
+import css from "../pages/"
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
@@ -56,8 +24,16 @@ const MovieDetailsPage = () => {
 
   return (
       <div>
-          <img src={ `https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} height={280} width={180}/>
-      <h4>{movie.title}</h4>
+          <div className={css.poster}>
+              <div> <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} height={280} width={180} /></div>
+         <div> <h4>{movie.title}</h4>
+          <p>Vote count: {movie.vote_count} </p>
+          <h4>Overview</h4>
+          <p>{movie.overview}</p>
+          <h4>Release date</h4>
+              <p>{movie.release_date}</p>
+              </div>
+          </div>
     
     </div>
   );
