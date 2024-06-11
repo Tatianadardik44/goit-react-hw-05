6fbb5da56897751350464f6b483ea9b667be228a
Tatianadardik44 <dardiktatiana@gmail.com>
2024-06-11 15:07:42 +0200

    add-movie

--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -18,7 +18,7 @@ const MovieDetailsPage = () => {
   const [movie, setMovie] = useState(null);
   const [loading, setLoading] = useState(false);
    const [error, setError] = useState(false);
-  const backLink = useRef(location.state?.from ?? '/');
+  const backLink = useRef(location.state?.from ?? '/movies');
   
   useEffect(() => {
     const getData = async () => {
